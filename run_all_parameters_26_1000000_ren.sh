4e0f5c7e9ce2eba0632c46652c94de3da9ac07af
Samantha Elaine Hardin <shardin31@login-phoenix-rh9-3.pace.gatech.edu>
2025-05-21 20:33:27 -0400

    update runs to include parameters nebular emission

--- run_all_parameters_26_1000000_ren.sh
+++ run_all_parameters_26_1000000_ren.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+#path  = "/storage/home/hcoda1/7/shardin31/p-jw254-0/Research/Powderday/powderday/parameters_master_"
+
+#model_files = "/storage/home/hcoda1/7/shardin31/p-jw254-0/Research/Powderday/powderday/parameters_model_*"
+
 for i in {725..754..1}
 do
-	python pd_front_end.py /storage/home/hcoda1/7/shardin31/p-jw254-0/Research/Powderday/powderday/parameter_files_renaissance/parameter_files_26 parameters_master_${i}_1000000 parameters_model_${i}_1000000 
+	python pd_front_end.py /storage/home/hcoda1/7/shardin31/p-jw254-0/Research/Powderday/powderday/parameter_files_renaissance/parameter_files_26 parameters_master_${i}_1000000 parameters_master_neb_${i}_1000000 parameters_model_${i}_1000000 
 done	
--- run_all_parameters_42_1000000_ren.sh
+++ run_all_parameters_42_1000000_ren.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+#path  = "/storage/home/hcoda1/7/shardin31/p-jw254-0/Research/Powderday/powderday/parameters_master_"
+
+#model_files = "/storage/home/hcoda1/7/shardin31/p-jw254-0/Research/Powderday/powderday/parameters_model_*"
+
 for i in {1189..1218..1}
 do
-	python pd_front_end.py /storage/home/hcoda1/7/shardin31/p-jw254-0/Research/Powderday/powderday/parameter_files_renaissance/parameter_files_42 parameters_master_${i}_1000000 parameters_model_${i}_1000000 
+	python pd_front_end.py /storage/home/hcoda1/7/shardin31/p-jw254-0/Research/Powderday/powderday/parameter_files_renaissance/parameter_files_42 parameters_master_${i}_1000000 parameters_master_neb_${i}_1000000 parameters_model_${i}_1000000 
 done	
